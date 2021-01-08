b0d7aa23d894c953893d8bbad8a25999922b252b
Boyzmsc <boyzmsc@naver.com>
2021-01-08 22:04:08 +0900

    feat: Add props-event

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
-    <app-header v-bind:propsdata="str"></app-header>
+    <app-header v-bind:propsdata="str" v-on:renew = "renewString"></app-header>
   </div>
 </template>
 
@@ -16,7 +16,13 @@ export default {
   },
   components:{
     'app-header' : AppHeader
+  },
+  methods : {
+    renewString: function(){
+      this.str = 'Noooo';
+    }
   }
+
 }
 </script>
 
--- vue-cli/src/components/AppHeader.vue
+++ vue-cli/src/components/AppHeader.vue
@@ -1,12 +1,18 @@
 <template>
   <header>
       <h1>{{propsdata}}</h1>
+      <button v-on:click="sendEvent">send</button>
   </header>
 </template>
 
 <script>
 export default {
-    props : ['propsdata']
+    props : ['propsdata'],
+    methods : {
+        sendEvent : function(){
+            this.$emit('renew');
+        }
+    }
 }
 </script>
 
