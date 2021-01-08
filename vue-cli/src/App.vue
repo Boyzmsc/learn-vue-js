a7726a46a89fd02705017ae40e5a08e3759253e3
Boyzmsc <boyzmsc@naver.com>
2021-01-08 21:55:43 +0900

    feat: Update App.vue & AppHeader.vue

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <app-header></app-header>
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str"></app-header>
   </div>
 </template>
 
@@ -10,7 +11,7 @@ import AppHeader from './components/AppHeader.vue';
 export default {
   data: function(){
     return {
-      str: 'hi'
+      str: 'Yess'
     }
   },
   components:{
--- vue-cli/src/components/AppHeader.vue
+++ vue-cli/src/components/AppHeader.vue
@@ -1,12 +1,12 @@
 <template>
   <header>
-      <h1>Header</h1>
+      <h1>{{propsdata}}</h1>
   </header>
 </template>
 
 <script>
 export default {
-
+    props : ['propsdata']
 }
 </script>
 
