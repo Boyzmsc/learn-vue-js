99e2764aee9c4a604be6c1184110fe37fae769e1
Boyzmsc <boyzmsc@naver.com>
2021-01-08 21:47:16 +0900

    feat: Update App.vue & Add AppHeader.vue

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,15 +1,20 @@
 <template>
   <div>
-    {{str}}
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
   data: function(){
     return {
       str: 'hi'
     }
+  },
+  components:{
+    'app-header' : AppHeader
   }
 }
 </script>
---
+++ vue-cli/src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+      <h1>Header</h1>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
