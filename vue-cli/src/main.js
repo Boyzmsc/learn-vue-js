149c2b4c3ef1461c86340e64e6e7f92a8a97e90c
Boyzmsc <boyzmsc@naver.com>
2021-01-08 20:43:11 +0900

    feat: Update main.js

--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 
 Vue.config.productionTip = false
 
+var App = {
+  template: '<div>app</div>'
+}
+
 new Vue({
   render: h => h(App),
+  components: {
+    'app' : App
+  }
 }).$mount('#app')
+
+// =
+// new Vue({
+//   el: '#app',
+//   render: h => h(App)
+// })
