f67a5b95689a1b48ed9ca0c9e25ec2fdfd1bd992
Boyzmsc <boyzmsc@naver.com>
2021-01-09 22:28:33 +0900

    feat: Add form

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -1,5 +1,14 @@
 <template>
-  
+  <form action="">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" type="text">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" type="password">
+    </div>
+  </form>
 </template>
 
 <script>
