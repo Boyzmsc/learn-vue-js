3ff503e215b21be3a5e7f0abce71e86bdc968dcf
Boyzmsc <boyzmsc@naver.com>
2021-01-09 22:43:06 +0900

    feat: Add v-model

--- vue-form/src/App.vue
+++ vue-form/src/App.vue
@@ -2,18 +2,27 @@
   <form action="">
     <div>
       <label for="username">id: </label>
-      <input id="username" type="text">
+      <input id="username" type="text" v-model="username">
     </div>
     <div>
       <label for="password">pw: </label>
-      <input id="password" type="password">
+      <input id="password" type="password" v-model="password">
     </div>
+    <button v->login</button>
   </form>
 </template>
 
 <script>
 export default {
+  data: function(){
+    return {
+      username : '',
+      password : ''
+    }
+  },
+  methods: {
 
+  }
 }
 </script>
 
